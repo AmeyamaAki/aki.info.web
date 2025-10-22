72e8f87f0b6bf895841b7efcf4389573fc8c3895
AmeyamaAki <git@aki.cat>
2025-10-22 16:46:36 +0800

    [fix] izumino's avatar url

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'izumino.cat',
+        hostname: 'blog.izumino.cat',
         pathname: '/**',
       },
       {
